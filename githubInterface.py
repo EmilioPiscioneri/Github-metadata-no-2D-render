27ff8758c111fd43612459c454c3e17d9bfe3250
EmilioPiscioneri <emilio.piscioneri@gmail.com>
2024-01-27 16:22:35 +1100

    updated stuff

--- githubInterface.py
+++ githubInterface.py
@@ -1,10 +1,20 @@
 import requests
+from pathlib import Path
+currentDirectory = Path("X:\python-programming\Github metadata no 2D render")
+import json
+configFilePath = currentDirectory / "config.json.gitignore" #config file path whicgh is ignore by github
+configFileObject = open(Path(configFilePath),"r") # open the config file
+rawConfigText = configFileObject.read()
+configFileJSON = json.loads(rawConfigText) # get clonfig file as json
+apikey = configFileJSON["api_key"]
+configFileObject.close()
+
 apiurl = "https://api.github.com"
 def githubGetRequest(url):
      headers = {
             "accept": "*/*",
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-            "Authorization": "nuh uh"
+            "Authorization": apikey
         }
      return  requests.get(url,headers)
 
